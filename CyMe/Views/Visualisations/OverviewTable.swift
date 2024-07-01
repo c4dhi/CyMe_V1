f64ac36bcbcead919c3cdbc050e32eea1877d923
Mariinja <91197819+Mariinja@users.noreply.github.com>
2024-07-01 21:15:54 +0200

    fix: big visualisation and add cycle slider

---
+++ CyMe/Views/Visualisations/OverviewTable.swift
@@ -0,0 +1,208 @@
+//  OverviewTable.swift
+//  CyMe
+//
+//  Created by Marinja Principe on 03.07.24.
+//
+
+import SwiftUI
+
+struct OverviewTable: View {
+    var symptoms: [SymptomModel]
+    var count = 1
+    
+    var body: some View {
+        ScrollView(.vertical){
+            ScrollView(.horizontal){
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Cycle days overview")
+                            .font(.headline)
+                            .padding(.bottom, 5)
+                        Spacer()
+                    }
+                    
+                    HStack {
+                        Text("Cycle day").frame(width: 100)
+                        ForEach(0..<minDays()-1, id: \.self) { day in
+                            Text("\(day + 1)")
+                                .frame(minWidth: 30)
+                                .frame(maxWidth: .infinity)
+                                .font(.caption)
+                        }
+                    }
+                    .padding()
+                    Divider().background(Color.gray)
+                    
+                    // Rows for each symptom
+                    ForEach(Array(symptoms.enumerated()), id: \.element.id) { (index, symptom) in
+                        HStack {
+                            Text(symptom.title)
+                                .frame(minWidth: 110)
+                                .font(.caption)
+                            
+                            ForEach(0..<minDays() - 1, id: \.self) { day in
+                                if let intensity = symptom.cycleOverview[day] {
+                                    getSymbol(for: symptom.questionType, intensity: intensity ?? 0)
+                                        .frame(maxWidth: .infinity)
+                                } else {
+                                    Text("-")
+                                        .frame(maxWidth: .infinity)
+                                }
+                            }
+                        }
+                        .padding()
+                        Divider().background(Color.gray)
+                    }
+                }
+                .padding()
+            }
+        }
+    }
+
+    private func minDays() -> Int {
+        print(symptoms)
+        return symptoms.map { $0.cycleOverview.count }.min() ?? 0
+    }
+    
+    private func getSymbol(for questionType: QuestionType, intensity: Int) -> AnyView {
+        switch questionType {
+        case .emoticonRating:
+            return AnyView(
+                EmoticonSymbol(emotion: intensity)
+            )
+        case .amountOfhour:
+            return AnyView(
+                Text("\(intensity)h")
+                    .font(.system(size: 10))
+                    .foregroundColor(.blue)
+            )
+        case .menstruationEmoticonRating:
+            return AnyView(
+                Circle()
+                    .fill(color(for: intensity, color: Color.red))
+                    .frame(width: circleSize(for: intensity), height: circleSize(for: intensity))
+            )
+        default:
+            return AnyView(
+                Circle()
+                    .fill(color(for: intensity, color: Color.blue))
+                    .frame(width: circleSize(for: intensity), height: circleSize(for: intensity))
+            )
+        }
+    }
+    
+    private func circleSize(for intensity: Int) -> CGFloat {
+        switch intensity {
+        case 1:
+            return 6
+        case 2:
+            return 12
+        case 3:
+            return 18
+        default:
+            return 0
+        }
+    }
+    
+    private func color(for intensity: Int, color: Color) -> Color {
+        switch intensity {
+        case 1:
+            return color.opacity(0.5)
+        case 2:
+            return color.opacity(0.7)
+        case 3:
+            return color.opacity(1.0)
+        default:
+            return Color.clear
+        }
+    }
+    
+    struct EmoticonSymbol: View {
+        let emotion: Int
+
+        let emoticons: [(String, String)] = [
+            ("😭", "Very Sad"),
+            ("😣", "Sad"),
+            ("🤔", "Neutral"),
+            ("😌", "Happy"),
+            ("🤩", "Very Happy")
+        ]
+
+        var body: some View {
+            if 0..<emoticons.count ~= emotion {
+                let (emoticon, _) = emoticons[emotion]
+                return Text(emoticon)
+                    .font(.system(size: 18))
+            } else {
+                return Text("")
+            }
+        }
+    }
+}
+
+struct OverviewTable_Previews: PreviewProvider {
+    static var previews: some View {
+        OverviewTable(symptoms: [
+            SymptomModel(
+                title: "Headache",
+                dateRange: [],
+                cycleOverview: [0, 1, 2, 3, 2, 1, 0, 1, 2, 3, 2, 1, 0, 1, 2, 3, 2, 1, 0, 1, 2, 3, 2, 1, 0, 1, 2, 3, 2, 1],
+                hints: ["Most frequent in period phase"],
+                min: "0",
+                max: "3",
+                average: "1",
+                covariance: 2.5,
+                covarianceOverview: [[2, 3, 4, 6, 5], [1, 2, 3, 4, 5]],
+                questionType: .painEmoticonRating
+            ),
+            SymptomModel(
+                title: "Fatigue",
+                dateRange: [],
+                cycleOverview: [1, 2, 3, 4, 3, 2, 1, 2, 3, 4, 3, 2, 1, 2, 3, 4, 3, 2, 1, 2, 3, 4, 3, 2, 1, 2, 3, 4, 3, 2],
+                hints: ["Most frequent in luteal phase"],
+                min: "1",
+                max: "4",
+                average: "2",
+                covariance: 1.8,
+                covarianceOverview: [[1, 2, 3, 4, 3], [2, 3, 4, 3, 2]],
+                questionType: .painEmoticonRating
+            ),
+            SymptomModel(
+                title: "Menstruation",
+                dateRange: [],
+                cycleOverview: [1, 2, 3, 4, 3, 2, 1, 2, 3, 4, 3, 2, 1, 2, 3, 4, 3, 2, 1, 2, 3, 4, 3, 2, 1, 2, 3, 4, 3, 2],
+                hints: ["Most frequent in luteal phase"],
+                min: "1",
+                max: "4",
+                average: "2",
+                covariance: 1.8,
+                covarianceOverview: [[1, 2, 3, 4, 3], [2, 3, 4, 3, 2]],
+                questionType: .menstruationEmoticonRating
+            ),
+            SymptomModel(
+                title: "Mood",
+                dateRange: [],
+                cycleOverview: [1, 2, 3, 4, 3, 2, 1, 2, 3, 4, 3, 2, 1, 2, 3, 4, 3, 2, 1, 2, 3, 4, 3, 2, 1, 2, 3, 4, 3, 2],
+                hints: ["Most frequent in luteal phase"],
+                min: "1",
+                max: "4",
+                average: "2",
+                covariance: 1.8,
+                covarianceOverview: [[1, 2, 3, 4, 3], [2, 3, 4, 3, 2]],
+                questionType: .emoticonRating
+            ),
+            SymptomModel(
+                title: "Sleep",
+                dateRange: [],
+                cycleOverview: [1, 2, 3, 4, 3, 2, 1, 2, 3, 4, 3, 2, 1, 2, 3, 4, 3, 2, 1, 2, 3, 4, 3, 2, 1, 2, 3, 4, 3, 2],
+                hints: ["Most frequent in luteal phase"],
+                min: "1",
+                max: "4",
+                average: "2",
+                covariance: 1.8,
+                covarianceOverview: [[1, 2, 3, 4, 3], [2, 3, 4, 3, 2]],
+                questionType: .amountOfhour
+            )
+        ])
+    }
+}
